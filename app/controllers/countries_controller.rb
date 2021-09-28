364ced7f4380d230a8be7357e0de53adf8978eef
slicky1 <okunonke@gmail.com>
2021-09-28 04:25:19 -0400

    Country index route created

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,7 @@
 class CountriesController < ApplicationController
+    def index
+        countries = Country.all
+        render json: countries, only: [:name, :img_source]
+        # render json: countries
+      end
 end
